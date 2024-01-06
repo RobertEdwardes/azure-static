e930d04dc8b0913252612445f14c2a3a0b306f2d
Robert Edwardes <rdedwardes@gmail.com>
2024-01-06 12:56:02 -0500

    basic example of azure function returning html

--- api/function_app.py
+++ api/function_app.py
@@ -1,8 +1,9 @@
 import azure.functions as func
+import mimetypes
 import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
-
+filename = 'index.html'
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -19,7 +20,6 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        with open(filename, 'rb') as f:
+            mimetype = mimetypes.guess_type(filename)
+            return func.HttpResponse(f.read(), mimetype=mimetype[0])
